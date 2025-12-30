3b5423895880f9ab28aec47fdee6f13a0133e898
Tridib11 <tridibghosh12345@gmail.com>
2025-12-30 13:48:17 +0530

    Day 1 completed

--- The Complete Python Pro Bootcamp/Day 1/task/task.py
+++ The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -1,5 +1,4 @@
-# name=input("What is your name?\n")
-# print(name)
-
-
-print("Hello "+input("what is your name \n"))
+print("Welcome to the Band Name Generator")
+city=input("Which city did you grow up in?\n")
+pet=input("What is the name of a pet?\n")
+print("Your band name could be: "+city+" "+pet)
