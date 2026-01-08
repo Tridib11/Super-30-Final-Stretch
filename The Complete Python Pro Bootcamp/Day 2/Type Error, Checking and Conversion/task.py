fadc05ea1e1fcba1cae2e144a3568031ad58b093
Tridib11 <tridibghosh12345@gmail.com>
2026-01-08 11:56:30 +0530

    Update task status to 'Solved' and refactor name length calculation in task.py

--- The Complete Python Pro Bootcamp/Day 2/Type Error, Checking and Conversion/task.py
+++ The Complete Python Pro Bootcamp/Day 2/Type Error, Checking and Conversion/task.py
@@ -2,4 +2,6 @@
 #
 # print(int("123")+int("345"))
 
-print("Number of letters in your name: " + len(input("Enter your name")))
+name_of_the_user=input("Enter your name: ")
+number_of_letters=len(name_of_the_user)
+print("Number of letters in your name: " + str(number_of_letters))
